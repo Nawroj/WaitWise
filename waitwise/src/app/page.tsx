3cf411efbf481bae07d03d5b7f01b7c6242bad0f
Nawroj <82nawroj@gmail.com>
2025-07-13 21:33:57 +1000

    fix

--- waitwise/src/app/page.tsx
+++ waitwise/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { MobileNav } from "@/components/ui/MobileNav";
